b3bc59c86c30f9f856a5f2c114206c055e698244
Michael Doleman <doleman.michael@gmail.com>
2024-01-02 09:57:12 -0800

    More schema additions, adding some services

---
+++ src/part.service.ts
@@ -0,0 +1,7 @@
+import {PrismaClient, Prisma} from '@prisma/client';
+
+class PartService {
+    static prisma = new PrismaClient();
+
+
+}
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import {PrismaClient, Prisma} from '@prisma/client';
+
+class Server {
+    prisma = new PrismaClient();
+}
