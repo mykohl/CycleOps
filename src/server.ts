1e3ce74db57571bd5ce846906f4e85c1aa109b41
Michael Doleman <doleman.michael@gmail.com>
2024-01-06 09:27:53 -0800

    moved prisma folder

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./api/prisma/@prisma/client";
 import PartsRouter from "./api/routes/parts.route";
 import MakerRouter from "./api/routes/maker.route";
 
