8dbfbd6e1a0ee839243c6ae1fa7640c7036171d1
Michael Doleman <doleman.michael@gmail.com>
2024-02-01 11:56:03 -0800

    reconfiguring styles, got user management more or less done.

--- cycle-ops-app/src/app/features/admin/user/user.component.ts
+++ cycle-ops-app/src/app/features/admin/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { ApiReqUserService } from '../../../shared/services/api-request-services/user-request-service/api-req-user.service';
 import { UserDto } from '../../../../../../data/models/model.dto';
@@ -21,32 +22,21 @@ import { MatSort } from '@angular/material/sort';
   ]
 })
 export class UserComponent {
-  @ViewChild(MatPaginator) set matPaginator(paginator: MatPaginator) {
-    this._paginator = paginator;
-    if(this.userDataSource) this.userDataSource.paginator = this._paginator;
-  } 
-  @ViewChild(MatSort) set matSort(sort: MatSort) {
-    this._sort = sort;
-    if(this.userDataSource) this.userDataSource.sort = this._sort;
-  }
+  @ViewChild(MatPaginator) paginator = {} as MatPaginator;
+  @ViewChild(MatSort) sort = {} as MatSort;
 
-  private _paginator: MatPaginator;
-  private _sort: MatSort;
-  private _userList: UserDto[];
   private _expandedUser: UserDto | null;
 
-  userDataSource: MatTableDataSource<UserDto>;
-  columnDef = [
-    { key: 'id', title: 'Id', sort: false },
-    { key: 'nameLast', title: 'Last Name', sort: true },
-    { key: 'nameFirst', title: 'First Name', sort: true },
-    { key: 'role', title: 'Role', sort: false },
-    { key: 'registered', title: 'Registered On', sort: false }
-  ]
+  userDataSource: MatTableDataSource<UserDto> = new MatTableDataSource();
+
+  tableColumns = [
+    'id','nameLast','nameFirst','role','registered','lastLogIn'
+  ];
+
+  expandedColumns = [...this.tableColumns, "expandAction"]
+
   availableRoles = [
-    "admin",
-    "standard",
-    "disabled"
+    "admin", "standard", "disabled"
   ]
 
   constructor(
@@ -55,10 +45,11 @@ export class UserComponent {
     private _snackBar: MatSnackBar) {
   }
 
-  ngOnInit() {
-    const userFetch = this._apiReqUserService.getUsers().subscribe((result) => {
-      this._userList = result;
-      this.userDataSource = new MatTableDataSource(result);
+  ngAfterViewInit() {
+    this.fetchUserData().subscribe(userData => {
+      this.userDataSource.data = userData;
+      this.userDataSource.paginator = this.paginator;
+      this.userDataSource.sort = this.sort;
     });
   }
 
@@ -100,31 +91,16 @@ export class UserComponent {
       this._expandedUser.role = newRole;
       this._apiReqUserService.updateUser(this._expandedUser).subscribe((result) => {
         this._snackBar.open(
-          `Role updated for ${this._expandedUser?.name}`, 'Dismiss',
+          `Role updated for ${this._expandedUser?.name}`, 
+          'Dismiss',
           { duration: 3600 }
         );
       });
     }
   }
 
-  get userList(): UserDto[] {
-    return this._userList;
-  }
-
-  get displayColumns(): string[] {
-    return this.columnDef.map(c => c.key);
-  }
-
-  get displayColumnsExpanded(): string[] {
-    return [...this.displayColumns, 'expand'];
-  }
-
-  findColumnTitle(key: string): string | undefined {
-    return this.columnDef.find(c => c.key === key)?.title
-  }
-
-  isSortable(key: string): boolean {
-    return this.columnDef.find(c => c.key === key)?.sort;
+  fetchUserData(): Observable<UserDto[]> {
+    return this._apiReqUserService.getUsers();
   }
 
   applyFilter(event: Event) {
